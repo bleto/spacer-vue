277216db2d41485b2ce378df31cbe4ff979f78c2
Piotr Bletek <p.bletek@gmail.com>
2018-08-28 21:06:21 +0200

    Add background and components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,60 @@
 <template>
   <div class="app">
-    <header>
+    <header class="header">
       <nav class="nav">
-        <router-link to="/">Spacer</router-link> |
-        <router-link to="/about">About</router-link>
+        <router-link to="/" class="link logo">Spacer</router-link>
+        <router-link to="/about" class="link about">@bout</router-link>
       </nav>
     </header>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss" scoped />
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,700');
+  *{ 
+    box-sizing: border-box
+  }
+  body{
+    margin:0;
+    padding:0;
+    font-family: 'Montserrat', sans-serif;
+  }
+  .app{
+    margin: 0;
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    background: url('./assets/bg.jpg') 0 40% / cover no-repeat;
+
+    .header{
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 40px;
+
+      .link{
+        font-size: 16px;
+        text-decoration: none;
+        padding: 20px;
+        position: absolute;
+        top:0;
+        left: 0;
+
+        &.logo{
+          color:#000;
+          font-weight: 700;
+          font-size: 30px;
+
+        } 
+        &.about{
+          right: 0;
+          left: unset;
+          color: rgb(236, 12, 61);
+        } 
+      }
+      
+    }
+  }
+</style>
---
+++ src/components/Claim.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="claimWrapper">
+    <h1 class="claim">Spacer</h1>
+    <p class="subclaim">
+        jakieś info bla bl bal
+    </p>
+  </main>
+</template>
+<script>
+  export default {
+    name: 'Claim'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .claimWrapper{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center
+  }
+  .claim{
+      margin: 0;
+      font-size: 50px;
+  }
+  .subclaim{
+      margin: 20px 0 0 0;
+      font-size: 13px;
+  }
+</style>
